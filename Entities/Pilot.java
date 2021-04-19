83fb4ce8becf49afb0879dabc18519bac82d7aef
jgabrantes <jgabrantes@ua.pt>
2021-04-19 10:00:17 +0100

    Pilot life cycle

--- Entities/Pilot.java
+++ Entities/Pilot.java
@@ -9,6 +9,7 @@ public class Pilot {
     private DepartureAirport depAirport;
     private DestinationAirport destAirport;
     private Plane  plane;
+    private PilotState state;
     public Pilot(int id, DepartureAirport depAirport, DestinationAirport destAirport, Plane plane) {
         this.id = id;
         this.depAirport = depAirport;
@@ -21,6 +22,31 @@ public class Pilot {
     public void setId(int id) {
         this.id = id;
     }
-
     
+    public PilotState getState() {
+        return state;
+    }
+
+    public void setState(PilotState state) {
+        this.state = state;
+    }
+    
+     public void run() {
+        System.out.println("Pilot start:");
+        while(depAirport.waitForNextFlight()){
+            setState(state.AT_TRANFER_GATE);
+            depAirport.informPlaneReadyForBoarding();
+            setState(state.READY_FOR_BOARDING);
+            depAirport.waitForAllInBoard();
+            setState(state.WAIT_FOR_BOARDING);
+            plane.flyToDestinationPoint();
+            setState(state.FLYING_FORWARD);
+            destAirport.announceArrival();
+            setState(state.DEBOARDING);
+            plane.flyToDeparturePoint();
+            setState(state.FLYING_BACK);
+            depAirport.parkAtTransfeGate();
+            
+        }
+    }
 }
