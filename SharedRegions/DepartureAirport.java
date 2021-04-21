eeca12dbeb4032e1aa06049b13d31119e519f3a8
catiaiazevedo <catiaiazevedo@ua.pt>
2021-04-21 18:30:55 +0100

    DepartureTesting

--- SharedRegions/DepartureAirport.java
+++ SharedRegions/DepartureAirport.java
@@ -17,138 +17,200 @@ public class DepartureAirport {
     private Queue<Integer> passengerQueue;
     private boolean readyToBoard,inQueue,checkDocs,docShown,board, readyToFly;
 
-    public synchronized void waitForNextFlight() {
-        Hostess hostess = (Hostess)Thread.currentThread();
-        hostess.setHostessState(HostessState.WAIT_FOR_FLIGHT);
-        while(!readyToBoard){
-            try{
-                wait();
-            }catch(InterruptedException e){
-                System.exit(1);
-            }
+    public synchronized void travelToAirport() 
+    {
+        Passenger passenger = ((Passenger)Thread.currentThread());
+        passenger.setPassengerState(PassengerState.GOING_TO_AIRPORT);
+        
+        System.out.println("Passenger"+passenger.getPassengerId()+"is traveling to the airport");
+        
+        try 
+        {
+        	passenger.sleep((long)(Math.random() * 1000));
+        } 
+        
+        catch (InterruptedException ex) 
+        {
+            System.exit(1);
         }
+        
+        System.out.println("Passenger"+passenger.getPassengerId()+"arrived");
     }
     
-
-    public synchronized void informPlaneReadyForBoarding() {
+    public synchronized void informPlaneReadyForBoarding() 
+    {
         Pilot pilot = ((Pilot)Thread.currentThread());
         pilot.setPilotState(PilotState.READY_FOR_BOARDING);
         readyToBoard = true;
         notifyAll();
-        
     }
-
-    public  synchronized  void waitForAllInBoard() {
+    
+    public  synchronized void waitInQueue() 
+    {
+        Passenger passenger = ((Passenger)Thread.currentThread());
+        passenger.setPassengerState(PassengerState.IN_QUEUE);    
+        passengerQueue.add(passenger.getPassengerId());
+        inQueue = true;        
+        notifyAll();
+    }
+    
+    public synchronized int prepareForPassBoarding() 
+    {
+        while(!inQueue)
+        {
+            try
+            {
+                wait();
+            }
+            
+            catch(InterruptedException e)
+            {
+            	System.exit(1);
+            }
+        }
+        
+        Hostess hostess = ((Hostess)Thread.currentThread());
+        hostess.setHostessState(HostessState.WAIT_FOR_PASSENGER);
+        
+        return passengerQueue.size();
+    }
+    
+    public  synchronized  void waitForAllInBoard() 
+    {
         Pilot pilot = ((Pilot)Thread.currentThread());
         pilot.setPilotState(PilotState.WAIT_FOR_BOARDING);
-        while(!readyToFly){
-            try{
+        
+        while(!readyToFly)
+        {
+            try
+            {
                 wait();
-            }catch(InterruptedException e){
+            }
+            
+            catch(InterruptedException e)
+            {
                 System.exit(1);
             }
         }
     }
-
-    public  synchronized  void parkAtTransfeGate() {
-        Pilot pilot = ((Pilot)Thread.currentThread());
-        pilot.setPilotState(PilotState.AT_TRANFER_GATE);
-    }
-
-    public  synchronized void waitInQueue() {
-        Passenger passenger = ((Passenger)Thread.currentThread());
-        passenger.setPassengerState(PassengerState.IN_QUEUE);        
-        inQueue = true;        
-        
-    }
-
-    public  synchronized  int prepareForPassBoarding() {      
-       
-        while(!inQueue){
-            try{
-                wait();
-            }catch(InterruptedException e){
-              System.exit(1);
-            }
-        }
-        Hostess hostess = ((Hostess)Thread.currentThread());
-        hostess.setHostessState(HostessState.WAIT_FOR_PASSENGER);
-        return passengerQueue.size();
-    }
-
-    public synchronized void checkDocuments() {
+    
+    public synchronized void checkDocuments() 
+    {
         Hostess hostess = ((Hostess)Thread.currentThread());
         hostess.setHostessState(HostessState.CHECK_PASSENGER);
         checkDocs = true;
-        while(!docShown){
-            try{
+        notifyAll();
+        
+        while(!docShown)
+        {
+            try
+            {
                 wait();
-            }catch(InterruptedException e){
+            }
+            
+            catch(InterruptedException e)
+            {
                 System.exit(1);
             }
         }
     }
+    
+    public  synchronized void showDocuments() 
+    {
+        Passenger passenger = ((Passenger)Thread.currentThread());
+        passenger.setPassengerState(PassengerState.IN_QUEUE);
+        
+        while(!checkDocs)
+        {
+            try
+            {
+                wait();
+            }
+            
+            catch(InterruptedException e)
+            {
+                System.exit(1);
+            }
+        }
+        
+        docShown=true;
+        notifyAll();
+    }
 
-    public synchronized int waitForNextPassenger() {
+    public synchronized int waitForNextPassenger() 
+    {
         Hostess hostess = ((Hostess)Thread.currentThread());
         hostess.setHostessState(HostessState.WAIT_FOR_PASSENGER);
         board = true;
-        while(!inQueue){
-            try{
+        passengerQueue.remove();
+        notifyAll();
+        
+        while(!inQueue)
+        {
+            try
+            {
                 wait();
-            }catch(InterruptedException e){
+            }
+            
+            catch(InterruptedException e)
+            {
                 System.exit(1);
             }
         }
-        board=true;
+        
+        
         return passengerQueue.size();
     }
-
-    public synchronized void informPlaneReatyToTakeOff() {
+    
+    public synchronized void boardThePlane() 
+    {
+        while(!board)
+        {
+            try
+            {
+                wait();
+            }
+            
+            catch(InterruptedException e)
+            {
+                System.exit(1);
+            }
+        }
+        
+        Passenger passenger = ((Passenger)Thread.currentThread());
+        passenger.setPassengerState(PassengerState.IN_FLIGHT);
+     }
+    
+    public synchronized void informPlaneReatyToTakeOff() 
+    {
         Hostess hostess = ((Hostess)Thread.currentThread());
         hostess.setHostessState(HostessState.READY_TO_FLY);
         readyToFly= true;
+        notifyAll();
     }
-
-    public  synchronized void showDocuments() {
-        Passenger passenger = ((Passenger)Thread.currentThread());
-        passenger.setPassengerState(PassengerState.IN_QUEUE);
-        while(!checkDocs){
-            try{
+    
+    public synchronized void waitForNextFlight() 
+    {
+        Hostess hostess = (Hostess)Thread.currentThread();
+        hostess.setHostessState(HostessState.WAIT_FOR_FLIGHT);
+        
+        while(!readyToBoard)
+        {
+            try
+            {
                 wait();
-            }catch(InterruptedException e){
+            }
+            
+            catch(InterruptedException e)
+            {
                 System.exit(1);
             }
         }
-        docShown=true;
-        
-        
     }
 
-    public synchronized void boardThePlane() {
-        
-       while(!board){
-           try{
-               wait();
-           }catch(InterruptedException e){
-               System.exit(1);
-           }
-       }
-       Passenger passenger = ((Passenger)Thread.currentThread());
-       passenger.setPassengerState(PassengerState.IN_FLIGHT);
-       
+    public  synchronized  void parkAtTransfeGate() 
+    {
+        Pilot pilot = ((Pilot)Thread.currentThread());
+        pilot.setPilotState(PilotState.AT_TRANFER_GATE);
     }
-
-    public synchronized void travelToAirport() {
-        Passenger passenger = ((Passenger)Thread.currentThread());
-        passenger.setPassengerState(PassengerState.GOING_TO_AIRPORT);
-        try {
-            passenger.sleep((long)(Math.random() * 1000));
-        } catch (InterruptedException ex) {
-            System.exit(1);
-        }
-        passengerQueue.add(passenger.getPassengerId());
-    }
-
-    
 }
